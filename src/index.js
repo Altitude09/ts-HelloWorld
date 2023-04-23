5adba16c048c7f60c91c991c780f235677c85fb0
Altitude09 <altitudeinfotech09@gmail.com>
2023-04-23 23:20:15 +0530

    Initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import { AppRuleProps, AppRuleType } from '../rules';
+import { PurpleUrlApp } from '../lib/purple-url-app';
+import { Env } from '../config';
+import { unimplemented } from '../utils/utils';
 console.log('Hello World');
 a = 7;
 b = 5;
