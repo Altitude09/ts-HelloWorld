187772c9749ca8d60a369834e8715def2e23b2f1
Altitude09 <altitudeinfotech09@gmail.com>
2023-05-01 10:49:18 +0530

    changes

--- src/add-search-api.ts
+++ src/add-search-api.ts
@@ -1,8 +1,7 @@
-
-import { PurpleUrlApp } from '../lib/purple-url-app';
-import { unimplemented } from '../utils/utils';
-import { AppRuleProps, AppRuleType } from '../rules';
 import { Env } from '../config';
+import { PurpleUrlApp } from '../lib/purple-url-app';
+import { AppRuleProps, AppRuleType } from '../rules';
+import { unimplemented } from '../utils/utils';
 
 export const addressSearchApiApp = new PurpleUrlApp('Address Search');
 
